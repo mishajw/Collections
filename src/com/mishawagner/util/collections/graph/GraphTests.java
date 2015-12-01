1654061c9081bc6ffcd220927c66f94fe500b712
Misha Wagner <mishajw@gmail.com>
2015-12-01 11:31:40 +0000

    TrieTree mostly implemented - still need to do deletion and compression

--- src/com/mishawagner/util/collections/graph/GraphTests.java
+++ src/com/mishawagner/util/collections/graph/GraphTests.java
@@ -6,69 +6,69 @@ import java.util.Random;
 import java.util.function.BiFunction;
 
 public class GraphTests {
-  private static final BiFunction<Point, Point, Double> distanceFunction =
-      (Point p1, Point p2) -> Math.sqrt(
-          Math.pow(p2.x - p1.x, 2) +
-              Math.pow(p2.y - p1.y, 2));
+    private static final BiFunction<Point, Point, Double> distanceFunction =
+            (Point p1, Point p2) -> Math.sqrt(
+                    Math.pow(p2.x - p1.x, 2) +
+                    Math.pow(p2.y - p1.y, 2));
 
-  private static final class Point {
-    final int x;
-    final int y;
+    private static final class Point {
+        final int x;
+        final int y;
 
-    public Point(int x, int y) {
-      this.x = x;
-      this.y = y;
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("(%d, %d)", x, y);
+        }
     }
 
-    @Override
-    public String toString() {
-      return String.format("(%d, %d)", x, y);
-    }
-  }
-
-  public static void main(String[] args) {
-    System.out.println("=== POINTERS ===");
-    testGraph(new PointerGraph<>());
-    System.out.println("\n=== MATRICES ===");
-    testGraph(new MatrixGraph<>());
-    System.out.println("\n=== ADJACENCY LIST ===");
-    testGraph(new AdjacencyListGraph<>());
-  }
-
-  private static void testGraph(AbstractGraph<Point> graph) {
-    // Create items
-    List<Point> items = new ArrayList<>();
-    for (int i = 0; i < 100; i++) {
-      for (int j = 0; j < 100; j++) {
-        items.add(new Point(i, j));
-      }
+    public static void main(String[] args) {
+        System.out.println("=== POINTERS ===");
+        testGraph(new PointerGraph<>());
+        System.out.println("\n=== MATRICES ===");
+        testGraph(new MatrixGraph<>());
+        System.out.println("\n=== ADJACENCY LIST ===");
+        testGraph(new AdjacencyListGraph<>());
     }
 
-    // Log start time for adding elements to the list
-    long addStart = System.currentTimeMillis();
+    private static void testGraph(AbstractGraph<Point> graph) {
+        // Create items
+        List<Point> items = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            for (int j = 0; j < 100; j++) {
+                items.add(new Point(i, j));
+            }
+        }
 
-    // Add them to the graph
-    graph.addAll(items);
+        // Log start time for adding elements to the list
+        long addStart = System.currentTimeMillis();
 
-    items = graph.getAllNodes();
+        // Add them to the graph
+        graph.addAll(items);
 
-    // Add random links
-    Random rand = new Random(1234);
-    for (int i = 1; i < items.size() * 5; i++) {
-      Point p1 = items.get(rand.nextInt(items.size()));
-      Point p2 = items.get(rand.nextInt(items.size()));
-      graph.addLink(p1, p2, distanceFunction.apply(p1, p2));
+        items = graph.getAllNodes();
+
+        // Add random links
+        Random rand = new Random(1234);
+        for (int i = 1; i < items.size() * 5; i++) {
+            Point p1 = items.get(rand.nextInt(items.size()));
+            Point p2 = items.get(rand.nextInt(items.size()));
+            graph.addLink(p1, p2, distanceFunction.apply(p1, p2));
+        }
+
+        // Print stats
+        System.out.println("Time to create: " + (System.currentTimeMillis() - addStart));
+        System.out.print("BFS: ");
+        graph.bfs(items.get(0), items.get(items.size() - 1));
+        System.out.print("DFS: ");
+        graph.dfs(items.get(0), items.get(items.size() - 1));
+        System.out.print("Dijstra: ");
+        graph.dijstra(items.get(0), items.get(items.size() - 1));
+        System.out.print("A*: ");
+        System.out.println(graph.aStar(items.get(0), items.get(items.size() - 1), distanceFunction));
     }
-
-    // Print stats
-    System.out.println("Time to create: " + (System.currentTimeMillis() - addStart));
-    System.out.print("BFS: ");
-    graph.bfs(items.get(0), items.get(items.size() - 1));
-    System.out.print("DFS: ");
-    graph.dfs(items.get(0), items.get(items.size() - 1));
-    System.out.print("Dijstra: ");
-    graph.dijstra(items.get(0), items.get(items.size() - 1));
-    System.out.print("A*: ");
-    System.out.println(graph.aStar(items.get(0), items.get(items.size() - 1), distanceFunction));
-  }
 }
--- src/com/mishawagner/util/collections/tree/TrieTree.java
+++ src/com/mishawagner/util/collections/tree/TrieTree.java
@@ -2,21 +2,43 @@ package com.mishawagner.util.collections.tree;
 
 import com.mishawagner.util.collections.Collection;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class TrieTree<T> extends Collection<List<T>> {
-  private class Node<U> {
-    U item;
-    List<U> children;
+  private T item;
+  private Set<TrieTree<T>> children;
 
-    public Node(U item) {
-      this.item = item;
-    }
+  public TrieTree(T item) {
+    this();
+    this.item = item;
+  }
+
+  public TrieTree() {
+    children = new HashSet<>();
   }
 
   @Override
   public void insert(List<T> ts) {
+    if (ts.size() == 0) {
+      return;
+    }
 
+    T head = ts.get(0);
+    List<T> tail = ts.subList(1, ts.size());
+
+    TrieTree<T> existingNode = getNodeForHead(head);
+
+    if (existingNode != null) {
+      existingNode.insert(tail);
+      return;
+    }
+
+    TrieTree<T> newChild = new TrieTree<>(head);
+    children.add(newChild);
+    newChild.insert(tail);
   }
 
   @Override
@@ -26,11 +48,97 @@ public class TrieTree<T> extends Collection<List<T>> {
 
   @Override
   public int size() {
-    return 0;
+    return 1 + children.stream()
+        .mapToInt(TrieTree::size)
+        .sum();
   }
 
   @Override
   public boolean contains(List<T> ts) {
-    return false;
+    if (ts.size() == 0) {
+      return true;
+    }
+
+    T head = ts.get(0);
+    List<T> tail = ts.subList(1, ts.size());
+
+    TrieTree<T> existingNode = getNodeForHead(head);
+
+    return existingNode != null && existingNode.contains(tail);
+  }
+
+  private TrieTree<T> getNodeForHead(T head) {
+    for (TrieTree<T> n : children) {
+      if (head == n.item) return n;
+    }
+
+    return null;
+  }
+
+  public List<List<T>> getAll() {
+    if (children.size() == 0) {
+      List<List<T>> all = new ArrayList<>();
+      List<T>       one = new ArrayList<>();
+      one.add(item);
+      all.add(one);
+      return all;
+    }
+
+    List<List<T>> all = new ArrayList<>();
+
+    for (TrieTree<T> n : children) {
+      List<List<T>> currentAll = n.getAll();
+
+      for (List<T> l : currentAll) {
+        l.add(0, item);
+      }
+
+      all.addAll(currentAll);
+    }
+
+    return all;
+  }
+
+  @Override
+  public String toString() {
+    String s = item + "(";
+
+    for (TrieTree<T> n : children) {
+      s += n.toString() + ", ";
+    }
+
+    s += ")";
+
+    return s;
+  }
+
+  public static void main(String[] args) {
+    TrieTree<Character> t = new TrieTree<>();
+
+    String[] elements = ("Shall I compare thee to a summer's day?\n" +
+        "Thou art more lovely and more temperate:\n" +
+        "Rough winds do shake the darling buds of May,\n" +
+        "And summer's lease hath all too short a date").split(" ");
+
+    for (String s : elements) {
+      t.insert(stringToCharList(s));
+    }
+
+    System.out.println(t.contains(stringToCharList(elements[0])));
+    System.out.println(t.contains(stringToCharList("world")));
+
+    System.out.println(t.getAll());
+
+    System.out.println(t);
+  }
+
+  private static List<Character> stringToCharList(String s) {
+    List<Character> list = new ArrayList<>();
+
+    for (Character c : s.toCharArray()) {
+      list.add(c);
+    }
+
+    return list;
   }
 }
