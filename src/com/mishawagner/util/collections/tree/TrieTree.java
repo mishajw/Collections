6d077a73c6354b613d5c0b783f3e38da4526fad5
Misha Wagner <mishajw@gmail.com>
2015-12-01 15:02:55 +0000

    TrieTree removal works, and the rest of it works with multiple elements

--- src/com/mishawagner/util/collections/tree/TrieTree.java
+++ src/com/mishawagner/util/collections/tree/TrieTree.java
@@ -1,6 +1,7 @@
 package com.mishawagner.util.collections.tree;
 
 import com.mishawagner.util.collections.Collection;
+import com.sun.org.apache.xml.internal.utils.Trie;
 
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -10,6 +11,7 @@ import java.util.Set;
 public class TrieTree<T> extends Collection<List<T>> {
   private T item;
   private Set<TrieTree<T>> children;
+  private int terminatedHere = 0;
 
   public TrieTree(T item) {
     this();
@@ -23,10 +25,14 @@ public class TrieTree<T> extends Collection<List<T>> {
   @Override
   public void insert(List<T> ts) {
     if (ts.size() == 0) {
+      terminatedHere++;
       return;
     }
 
     T head = ts.get(0);
+    if (head == null) {
+      throw new IllegalArgumentException("Can't add null elements.");
+    }
     List<T> tail = ts.subList(1, ts.size());
 
     TrieTree<T> existingNode = getNodeForHead(head);
@@ -43,19 +49,43 @@ public class TrieTree<T> extends Collection<List<T>> {
 
   @Override
   public boolean remove(List<T> ts) {
-    return false;
+    if (ts.size() == 0) {
+      if (terminatedHere > 0) {
+        terminatedHere--;
+        return true;
+      } else {
+        return false;
+      }
+    }
+
+    T head = ts.get(0);
+    List<T> tail = ts.subList(1, ts.size());
+
+    TrieTree<T> existingNode = getNodeForHead(head);
+
+    if (existingNode == null) {
+      return false;
+    }
+
+    boolean rest = existingNode.remove(tail);
+
+    if (rest && existingNode.isEmpty()) {
+      children.remove(existingNode);
+    }
+
+    return rest;
   }
 
   @Override
   public int size() {
-    return 1 + children.stream()
+    return terminatedHere + children.stream()
         .mapToInt(TrieTree::size)
         .sum();
   }
 
   @Override
   public boolean contains(List<T> ts) {
-    if (ts.size() == 0) {
+    if (ts.size() == 0 && terminatedHere > 0) {
       return true;
     }
 
@@ -76,21 +106,19 @@ public class TrieTree<T> extends Collection<List<T>> {
   }
 
   public List<List<T>> getAll() {
-    if (children.size() == 0) {
-      List<List<T>> all = new ArrayList<>();
-      List<T>       one = new ArrayList<>();
+    List<List<T>> all = new ArrayList<>();
+
+    for (int i = 0; i < terminatedHere; i++) {
+      List<T> one = new ArrayList<>();
       one.add(item);
       all.add(one);
-      return all;
     }
-
-    List<List<T>> all = new ArrayList<>();
 
     for (TrieTree<T> n : children) {
       List<List<T>> currentAll = n.getAll();
 
       for (List<T> l : currentAll) {
-        l.add(0, item);
+        if (item != null) l.add(0, item);
       }
 
       all.addAll(currentAll);
@@ -99,9 +127,23 @@ public class TrieTree<T> extends Collection<List<T>> {
     return all;
   }
 
+  public boolean isEmpty() {
+    if (terminatedHere > 0) {
+      return false;
+    }
+
+    for (TrieTree<T> t : children) {
+      if (!t.isEmpty()) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
   @Override
   public String toString() {
-    String s = item + "(";
+    String s = item + "[" + terminatedHere + "]" + "(";
 
     for (TrieTree<T> n : children) {
       s += n.toString() + ", ";
@@ -115,21 +157,40 @@ public class TrieTree<T> extends Collection<List<T>> {
   public static void main(String[] args) {
     TrieTree<Character> t = new TrieTree<>();
 
-    String[] elements = ("Shall I compare thee to a summer's day?\n" +
-        "Thou art more lovely and more temperate:\n" +
-        "Rough winds do shake the darling buds of May,\n" +
-        "And summer's lease hath all too short a date").split(" ");
+//    String[] elements = ("Shall I compare thee to a summer's day?\n" +
+//        "Thou art more lovely and more temperate:\n" +
+//        "Rough winds do shake the darling buds of May,\n" +
+//        "And summer's lease hath all too short a date").split(" ");
+
+    String[] elements = "abc abc abcd abc abcd".split(" ");
 
     for (String s : elements) {
       t.insert(stringToCharList(s));
     }
 
-    System.out.println(t.contains(stringToCharList(elements[0])));
-    System.out.println(t.contains(stringToCharList("world")));
-
+    System.out.println(t);
+    System.out.println(t.getAll());
+    System.out.println(t.remove(stringToCharList("abcd")));
+    System.out.println(t);
+    System.out.println(t.getAll());
+    System.out.println(t.remove(stringToCharList("abcd")));
+    System.out.println(t);
+    System.out.println(t.getAll());
+    System.out.println(t.remove(stringToCharList("abc")));
+    System.out.println(t);
+    System.out.println(t.getAll());
+    System.out.println(t.remove(stringToCharList("abc")));
+    System.out.println(t);
+    System.out.println(t.getAll());
+    System.out.println(t.remove(stringToCharList("abc")));
+    System.out.println(t);
+    System.out.println(t.getAll());
+    System.out.println(t.remove(stringToCharList("abc")));
+    System.out.println(t);
     System.out.println(t.getAll());
 
     System.out.println(t);
+    System.out.println(t.getAll());
   }
 
   private static List<Character> stringToCharList(String s) {
